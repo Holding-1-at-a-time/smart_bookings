25290b732339b0691303161e38bc693b2a67df59
Big Daddy Bobs <136872640+Holding-1-at-a-time@users.noreply.github.com>
2025-02-24 18:24:51 -0600

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,37 +1,21 @@
-/**
-    * @description      : 
-    * @author           : rrome
-    * @group            : 
-    * @created          : 23/02/2025 - 20:01:38
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 23/02/2025
-    * - Author          : rrome
-    * - Modification    : 
-**/
-
-"use client"
-import { SignIn } from "@clerk/nextjs"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+"use client";
+import { SignIn } from "@clerk/nextjs";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
-import { Button } from "@/components/ui/button";
 
 export default function SignInPage() {
-    const SignInButton = () => {
-        return (
-
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                <Card className="w-full max-w-md">
-                    <CardHeader>
-                        <CardTitle className="text-2xl font-bold text-center text-primary">Sign In</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <signIn />
-                    </Button>
-                </CardContent>
-            </Card>
-            </div >
-        )
-}
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold text-center text-primary">
+            Sign In
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SignIn />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
