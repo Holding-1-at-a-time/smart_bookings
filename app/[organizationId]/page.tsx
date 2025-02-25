98b15d084138a4ecd0183ff4b55205fab6134d37
Big Daddy Bobs <136872640+Holding-1-at-a-time@users.noreply.github.com>
2025-02-24 18:36:30 -0600

    Update page.tsx

--- app/[organizationId]/page.tsx
+++ app/[organizationId]/page.tsx
@@ -21,7 +21,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function HomePage() {
     const { organizationId } = useParams()
-    const businessInfo = useQuery   (api.organizationSettings.getOrganizationSettings);
+    const businessInfo = useQuery(api.organizationSettings.getOrganizationSettings);
     const featuredServices = useQuery(api.services.getFeaturedServices, { organizationId: organizationId as string })
 
     if (!businessInfo) {
